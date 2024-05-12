0625bca450dad248596b1418737a6b71ae99f054
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-05-12 20:25:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7031962678:AAHYFuJjK3dCdtm4LjJX3zySnzvBhByoai4")
+API_ID = int(os.environ.get("API_ID", "21257327"))
+API_HASH = os.environ.get("API_HASH", "1235c1fe45ebc4968d9e23bc93440549")
+OWNER = int(os.environ.get("OWNER", "5192808332"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "File_Renamer_4Gb_RoBot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002115299028"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://figega1249:owYb9NfJAuBRFFFV@cluster0.wfrsxjp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 STRING = os.environ.get("STRING", "")
@@ -28,9 +28,9 @@ BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae
 # token verification adding features is paid so if you want then dm me
 
 
-# SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
-# SHORTNER_API = os.environ.get("SHORTNER_API", "")
-# TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
+SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
+SHORTNER_API = os.environ.get("SHORTNER_API", "")
+TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
 
 
 # Jishu Developer 
